487cf6b8b18a811672ba80eb56fc7ed6a99c02c1
Matheus Justino <matheussjusttino@gmail.com>
2022-07-21 08:29:15 -0300

    feat: finishes the app design

--- src/index.js
+++ src/index.js
@@ -12,3 +12,19 @@ function component() {
 
 document.body.appendChild(component());
 */
+
+// STARTS HERE
+/*
+#1 get componentes need from DOM
+#2 get the key "enter press" and check whether the input is filled
+#3 call the API
+  #3.1 get route from API
+  #3.2 get API key
+  #3.3 call the API passing city name and API key
+#4 check whether the call was sucessed or failed
+  #4.1 get result
+#5 show correct datas in the screen
+
+
+
+*/
